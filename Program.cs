7fb0701524ab334561e2575bd4a05209d5409a46
Alceu Cardoso <contato@alceucardoso.com.br>
2025-02-06 14:07:58 -0300

    Implementação da soma de dois números

--- Program.cs
+++ Program.cs
@@ -5,11 +5,6 @@ Console.Write("Primeiro número: ");
 var n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Segundo número: ");
 var n2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Terceiro número: ");
-var n3 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine();
-Console.WriteLine($"Resultado é: {n1 + n2 + n3}");
-Console.WriteLine($"Resultado é: {n1 - n2 - n3}");
-Console.WriteLine($"Resultado é: {n1 / n2 / n3}");
-Console.WriteLine($"Resultado é: {n1 * n2 * n3}");
+Console.WriteLine($"Resultado é: {n1 + n2}");
